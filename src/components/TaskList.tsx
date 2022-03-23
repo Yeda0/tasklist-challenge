933d2a89a32c2b9509d76b25a95a0e105481aa6c
lucas.andrade <lucasgonaff@outlook.com>
2022-03-23 17:49:57 -0300

    remove and check task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,10 +16,10 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    
+    if(!newTaskTitle) return;
 
     const newTask : Task = {
-      id : Math.random(),
+       id: Math.floor(Math.random() * 100),
       title : newTaskTitle,
       isComplete : false
     }
@@ -38,11 +38,23 @@ export function TaskList() {
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     
-    
+    setTasks([
+      ...tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            isComplete: !task.isComplete,
+          };
+        }
+        return task;
+      }),
+    ]);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks([...tasks.filter((task)=> task.id != id)])
+
   }
 
   return (
